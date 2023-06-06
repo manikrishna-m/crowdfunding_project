1d099d9179e3bc60c8bbc1142ffe957deab77823
Mani Krishna <mandepudi.mk@gmail.com>
2023-06-06 13:46:08 +0100

    Made changes to the feature_engineering python files

--- src/data_pipelines/feature_engineering/feature_engineering_scripts.py
+++ src/data_pipelines/feature_engineering/feature_engineering_scripts.py
@@ -7,7 +7,7 @@ from src.exception import CustomException
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 data = pd.read_csv('data/processed/output_data.csv')
 
@@ -17,7 +17,10 @@ categorical_columns = data.select_dtypes(include=['object']).columns.tolist()
 try:
     preprocessor = ColumnTransformer(
         transformers=[
-            ('numeric', SimpleImputer(strategy='median'), numeric_columns),
+            ('numeric', Pipeline(steps=[
+                ('imputer', SimpleImputer(strategy='median')),
+                ('scaler', StandardScaler())
+            ]), numeric_columns),
             ('categorical', Pipeline(steps=[
                 ('imputer', SimpleImputer(strategy='most_frequent')),
                 ('encoder', OneHotEncoder())
